3458a2f6a0d0d5855ac3ca7985c3fbd9919809ed
Levente Bajczi <bajczilevi@gmail.com>
2019-10-01 22:57:55 +0200

    lab3

--- lab3/AndroidWallet/app/src/main/java/hu/bme/aut/androidwallet/MainActivity.java
+++ lab3/AndroidWallet/app/src/main/java/hu/bme/aut/androidwallet/MainActivity.java
@@ -1,5 +1,6 @@
 package hu.bme.aut.androidwallet;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,18 +9,56 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText nameEditText;
+    private EditText amountEditText;
+    private ToggleButton typeChooserButton;
+    private Button saveButton;
+    private LinearLayout listOfRows;
+    private LayoutInflater inflater;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        nameEditText = findViewById(R.id.salary_name);
+        amountEditText = findViewById(R.id.salary_amount);
+        typeChooserButton = findViewById(R.id.expense_or_income);
+        saveButton = findViewById(R.id.save_button);
+        listOfRows = findViewById(R.id.list_of_rows);
+
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (nameEditText.getText().toString().isEmpty() || amountEditText.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, R.string.warn_message, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                View rowItem = inflater.inflate(R.layout.salary_row, null);
+                ImageView icon = rowItem.findViewById(R.id.salary_direction_icon);
+                TextView rowItemSalaryName = rowItem.findViewById(R.id.row_salary_name);
+                TextView rowItemSalaryAmount = rowItem.findViewById(R.id.row_salary_amount);
+                icon.setImageResource(typeChooserButton.isChecked() ? R.drawable.expense : R.drawable.income);
+                rowItemSalaryName.setText(nameEditText.getText().toString());
+                rowItemSalaryAmount.setText(amountEditText.getText().toString());
+                listOfRows.addView(rowItem);
+            }
+        });
     }
 
     @Override
@@ -31,13 +70,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_delete_all) {
+            listOfRows.removeAllViews();
             return true;
         }
 
