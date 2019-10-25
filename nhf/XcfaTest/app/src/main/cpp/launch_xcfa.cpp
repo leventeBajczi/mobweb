83d8c87b4af72d0277f649f9b5197ef3955240fd
Levente Bajczi <bajczilevi@gmail.com>
2019-10-25 21:38:54 +0200

    added JNI code

--- nhf/XcfaTest/app/src/main/cpp/launch_xcfa.cpp
+++ nhf/XcfaTest/app/src/main/cpp/launch_xcfa.cpp
@@ -2,3 +2,10 @@
 // Created by levente on 10/25/19.
 //
 
+#include <jni.h>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_hu_bme_aut_xcfatest_MainActivity_textFromNative( JNIEnv* env, jobject thiz)
+{
+    return env -> NewStringUTF("Hello from native code!");
+}
